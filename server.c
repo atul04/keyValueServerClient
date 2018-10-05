d1bf25e5757c41f490bb806454c746fc685bdf35
atul04 <atulsahay01@gmail.com>
2018-10-05 17:04:07 +0530

    ++all clear ++threads done ++batch.txt running well ++long words done --need to check for corner case --clean code not done

--- server.c
+++ server.c
@@ -3,123 +3,10 @@
  * @Date:   2018-09-24T19:32:51+05:30
  * @Email:  atulsahay01@gmail.com
  * @Last modified by:   atul
- * @Last modified time: 2018-10-05T14:36:55+05:30
- */
-
-
-//
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <netdb.h>
-// #include <netinet/in.h>
-//
-// #include <string.h>
-//
-// void doprocessing (int sock);
-//
-// int main( int argc, char *argv[] ) {
-//    int sockfd, newsockfd, portno, clilen;
-//    char buffer[256];
-//    struct sockaddr_in serv_addr, cli_addr;
-//    int n, pid;
-//
-//    /* First call to socket() function */
-//    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-//
-//    if (sockfd < 0) {
-//       perror("ERROR opening socket");
-//       exit(1);
-//    }
-//
-//    /* Initialize socket structure */
-//    bzero((char *) &serv_addr, sizeof(serv_addr));
-//    portno = 5001;
-//
-//    serv_addr.sin_family = AF_INET;
-//    serv_addr.sin_addr.s_addr = INADDR_ANY;
-//    serv_addr.sin_port = htons(portno);
-//
-//    /* Now bind the host address using bind() call.*/
-//    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-//       perror("ERROR on binding");
-//       exit(1);
-//    }
-//
-//    /* Now start listening for the clients, here
-//       * process will go in sleep mode and will wait
-//       * for the incoming connection
-//    */
-//
-//    listen(sockfd,5);
-//    clilen = sizeof(cli_addr);
-//
-//    while (1) {
-//       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-//
-//       if (newsockfd < 0) {
-//          perror("ERROR on accept");
-//          exit(1);
-//       }
-//
-//       /* Create child process */
-//       pid = fork();
-//
-//       if (pid < 0) {
-//          perror("ERROR on fork");
-//          exit(1);
-//       }
-//
-//       if (pid == 0) {
-//          /* This is the client process */
-//          close(sockfd);
-//          printf("Servicing %d\n",clilen);
-//          doprocessing(newsockfd);
-//          exit(0);
-//       }
-//       else {
-//         // close(newsockfd);
-//       }
-//
-//    } /* end of while */
-// }
-// void doprocessing (int sock) {
-//       int n;
-//       char buffer[256];
-//       bzero(buffer,256);
-//       while(1){
-//       n = read(sock,buffer,255);
-//
-//       if (n < 0) {
-//          perror("ERROR reading from socket");
-//          exit(1);
-//       }
-//
-//       printf("Here is the message: %s\n",buffer);
-//       n = write(sock,"I got your message",18);
-//
-//       if (n < 0) {
-//          perror("ERROR writing to socket");
-//          exit(1);
-//       }
-//     }
-//
-//    }
-
-
-   // *** thread***
-
-   /**
- * @Author: Atul Sahay <atul>
- * @Date:   2018-09-02T15:31:43+05:30
- * @Email:  atulsahay01@gmail.com
- * @Filename: newUpdate.c
- * @Last modified by:   atul
- * @Last modified time: 2018-10-05T14:36:55+05:30
+ * @Last modified time: 2018-10-05T17:00:15+05:30
  */
 
  /*
-   Famous bounded buffer problem, with 1 producer and 4 consumer are
-   present. Following are the parameters and vars:
 
    1. MAXITERATIONS = macro used to control the numbers to produce by the
                        producer. For our case 10K.
@@ -151,6 +38,7 @@
   #define MAXBUFFERSIZE 3
   #define SLEEP_NANOSEC_PROD 100
   #define SLEEP_NANOSEC_CONS 100
+  #define CONSTHREAD 100
 
   /////////////////////      Global definitions
   pthread_mutex_t mutex;
@@ -223,7 +111,7 @@
     pthread_cond_init(&reader_can_enter, NULL);		/* Initialize reader condition variable */
     pthread_cond_init(&writer_can_enter, NULL);		/* Initialize writer condition variable */
     // Ends
-    pthread_t pro, con1,con2,con3,con4; // Pthreads
+    pthread_t pro, con[CONSTHREAD]; //con1,con2,con3,con4; // Pthreads
 
     // Networking things
 
@@ -278,21 +166,30 @@
 
     // Ids for every pthread
     int prod_thread_id = 0;
-    int con1_thread_id = 1;
-    int con2_thread_id = 2;
-    int con3_thread_id = 3;
-    int con4_thread_id = 4;
-
+    // int con1_thread_id = 1;
+    // int con2_thread_id = 2;
+    // int con3_thread_id = 3;
+    // int con4_thread_id = 4;
+    int con_thread_id[CONSTHREAD];
+    int i = 0;
+    for(i = 0 ; i < CONSTHREAD ; i++)
+    {
+        con_thread_id[i] = i+1;
+    }
     // Initialize the mutex and condition variables
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&condc, NULL);		/* Initialize consumer condition variable */
     pthread_cond_init(&condp, NULL);		/* Initialize producer condition variable */
     // Creation of threads
     pthread_create(&pro, NULL, producer, (void *)&prod_thread_id);
-    pthread_create(&con3, NULL, consumer, (void *)&con3_thread_id);
-    pthread_create(&con4, NULL, consumer, (void *)&con4_thread_id);
-    pthread_create(&con2, NULL, consumer, (void *)&con2_thread_id);
-    pthread_create(&con1, NULL, consumer, (void *)&con1_thread_id);
+    for(i = 0 ; i < CONSTHREAD ; i++)
+    {
+        pthread_create(&con[i], NULL, consumer, (void *)&con_thread_id[i]);
+    }
+    // pthread_create(&con3, NULL, consumer, (void *)&con3_thread_id);
+    // pthread_create(&con4, NULL, consumer, (void *)&con4_thread_id);
+    // pthread_create(&con2, NULL, consumer, (void *)&con2_thread_id);
+    // pthread_create(&con1, NULL, consumer, (void *)&con1_thread_id);
 
     sleep(1000);
     printf("Captured Error: Resources not allocated\n");
@@ -705,11 +602,12 @@
       // }
       newsockfd = release();
       printf("Thread %d Consumed %d Current Buffer size %d\n",thread_id,newsockfd,queue_count);
-      while(queue_count!=0)
-        pthread_cond_signal(&condp);
+      // doprocessing(newsockfd,thread_id); { when nothing works do this}
       pthread_cond_broadcast(&condc);
+      pthread_cond_signal(&condp);
       pthread_mutex_unlock(&mutex);
       doprocessing(newsockfd,thread_id);
+
       // optional sleep to allow workers to run
       struct timespec delay;
       delay.tv_sec = 0;
